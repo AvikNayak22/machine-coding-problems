c4257fde7b31eedca592d569ea247a2ade39ad19
AvikNayak <nayakavik19@gmail.com>
2024-06-19 16:31:08 +0530

    started practicing machine coding questions

---
+++ pagination/src/App.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchProducts = async () => {
+    const res = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+    );
+    const data = await res.json();
+
+    if (data && data.products) {
+      setProducts(data.products);
+      setTotalPages(Math.ceil(data.total / 10));
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [page]);
+
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= totalPages &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
+
+  return (
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.map((product) => (
+            <span className="products__single" key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <span>{product.title}</span>
+            </span>
+          ))}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="pagination">
+          <span
+            onClick={() => selectPageHandler(page - 1)}
+            className={page > 1 ? "" : "pagination__disable"}
+          >
+            ◀️
+          </span>
+          {[...Array(totalPages)].map((_, i) => (
+            <span
+              className={page === i + 1 ? "pagination__selected" : ""}
+              onClick={() => selectPageHandler(i + 1)}
+              key={i}
+            >
+              {i + 1}
+            </span>
+          ))}
+          <span
+            onClick={() => selectPageHandler(page + 1)}
+            className={page < totalPages ? "" : "pagination__disable"}
+          >
+            ▶️
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
