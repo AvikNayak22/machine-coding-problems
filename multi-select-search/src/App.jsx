9620d7116cc0b5aafe7b39a53bbe532c96e667fa
AvikNayak <nayakavik19@gmail.com>
2024-06-21 01:07:10 +0530

    multi-select-search problem added

---
+++ multi-select-search/src/App.jsx
@@ -0,0 +1,111 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Pill from "./components/Pill";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [selectedUserSet, setSelectedUserSet] = useState(new Set());
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const fetchUsers = () => {
+      if (searchTerm.trim() === "") {
+        setSuggestions([]);
+        return;
+      }
+
+      fetch(`http://dummyjson.com/users/search?q=${searchTerm}`)
+        .then((res) => res.json())
+        .then((data) => setSuggestions(data))
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+
+    fetchUsers();
+  }, [searchTerm]);
+
+  const handleSelectUser = (user) => {
+    setSelectedUsers([...selectedUsers, user]);
+    setSelectedUserSet(new Set([...selectedUserSet, user.email]));
+    setSearchTerm("");
+    setSuggestions([]);
+    inputRef.current.focus();
+  };
+
+  const handleRemoveUser = (user) => {
+    const updatedUsers = selectedUsers.filter(
+      (selectedUser) => selectedUser.id !== user.id
+    );
+
+    setSelectedUsers(updatedUsers);
+
+    const updatedEmails = new Set(selectedUserSet);
+    updatedEmails.delete(user.email);
+    setSelectedUserSet(updatedEmails);
+  };
+
+  const handleKeyDown = (e) => {
+    if (
+      e.key === "Backspace" &&
+      e.target.value === "" &&
+      selectedUsers.length > 0
+    ) {
+      const lastUser = selectedUsers[selectedUsers.length - 1];
+      handleRemoveUser(lastUser);
+      setSuggestions([]);
+    }
+  };
+
+  return (
+    <div className="user-search-container">
+      <div className="user-search-input">
+        {/* Pills */}
+        {selectedUsers.map((user) => {
+          return (
+            <Pill
+              key={user.email}
+              image={user.image}
+              text={`${user.firstName} ${user.lastName}`}
+              onClick={() => handleRemoveUser(user)}
+            />
+          );
+        })}
+        {/* input field with search suggestions */}
+        <div>
+          <input
+            ref={inputRef}
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for a user..."
+            onKeyDown={handleKeyDown}
+          />
+          {/* Search suggestions */}
+          <ul className="suggestions-list">
+            {suggestions?.users?.map((user) =>
+              !selectedUserSet.has(user.email) ? (
+                <li key={user.email} onClick={() => handleSelectUser(user)}>
+                  <img
+                    src={user.image}
+                    alt={`${user.firstName} ${user.lastName}`}
+                  />
+                  <span>
+                    {user.firstName} {user.lastName}
+                  </span>
+                </li>
+              ) : (
+                <></>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
